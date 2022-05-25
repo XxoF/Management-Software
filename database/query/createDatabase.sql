a572127a1bae67e91c85d8a83d7a0375dfa172d9
KhanhTa <khanhkglqd@gmail.com>
2022-05-25 17:37:08 +0700

    Create all database

--- database/query/createDatabase.sql
+++ database/query/createDatabase.sql
@@ -25,16 +25,69 @@ Insert Into [tblUsers] VALUES('User3', 'User Test 4', '123456', '1')
 
 
 CREATE TABLE [dbo].[tblProduct](
-	[ID] [nchar](20) NOT NULL PRIMARY KEY,
+	[ID] [int] NOT NULL PRIMARY KEY,
 	[Name] [nvarchar](50) NOT NULL,
 	[Price] [decimal](18,2) NOT NULL,
 	[Quantity] [decimal](10,0) NOT NULL ,
 )
 GO
 
-Insert Into [tblProduct] VALUES('1', 'Dep di trong nha', '50000', '100')
-Insert Into [tblProduct] VALUES('2', 'Chuot khong day', '69000', '30')
-Insert Into [tblProduct] VALUES('3', 'Ao thun nam', '45000', '10')
-Insert Into [tblProduct] VALUES('4', 'Day microphone', '10000', '30')
-Insert Into [tblProduct] VALUES('5', 'Iphone 13', '13000000', '15')
+Insert Into [tblProduct] VALUES(1, 'Dep di trong nha', '50000', '100')
+Insert Into [tblProduct] VALUES(2, 'Chuot khong day', '69000', '30')
+Insert Into [tblProduct] VALUES(3, 'Ao thun nam', '45000', '10')
+Insert Into [tblProduct] VALUES(4, 'Day microphone', '10000', '30')
+Insert Into [tblProduct] VALUES(5, 'Iphone 13', '13000000', '15')
 Insert Into [tblProduct] VALUES('6', 'Lot chuot may tinh', '15000', '200')
+GO
+
+
+CREATE TABLE [dbo].[tblOrder](
+	[ID] [int] NOT NULL PRIMARY KEY,
+	[CreateTime] [datetime] NOT NULL,
+	[OrderStatus] [nvarchar](50) NOT NULL,
+	[PaymentMethod] [nvarchar](50) NOT NULL,
+	[PaymentStatus] [bit] NOT NULL default 0,
+	[TransferStatus] [nvarchar](50) NOT NULL
+)
+GO
+
+
+
+CREATE TABLE [dbo].[tblOrderProduct](
+	[ID] [int] NOT NULL PRIMARY KEY,
+	[ProductID] [int] NOT NULL,
+	[OrderID] [int] NOT NULL,
+	[Quantity] [decimal](5,0) NOT NULL,
+	[Price] [decimal](18,2) NOT NULL,
+	FOREIGN KEY ([ProductID]) REFERENCES [tblProduct](ID),
+	FOREIGN KEY ([OrderID]) REFERENCES [tblOrder](ID),
+)
+GO
+
+
+CREATE TABLE [dbo].[tblDeliveryNote](
+	[ID] [int] NOT NULL PRIMARY KEY,
+	[CreateTime] [datetime] NOT NULL,
+	[Price] [decimal](18,2) NOT NULL,
+	[orderID] [int] NOT NULL ,
+	FOREIGN KEY ([OrderID]) REFERENCES [tblOrder](ID),
+)
+GO
+
+
+CREATE TABLE [dbo].[tblReceiveNote](
+	[ID] [int] NOT NULL PRIMARY KEY,
+	[CreateTime] [datetime] NOT NULL,
+)
+GO
+
+
+CREATE TABLE [dbo].[tblReceiveProduct](
+	[ID] [int] NOT NULL PRIMARY KEY,
+	[ProductID] [int] NOT NULL,
+	[ReceiveID] [int] NOT NULL,
+	[Quantity] [decimal](5,0) NOT NULL,
+	FOREIGN KEY ([ProductID]) REFERENCES [tblProduct](ID),
+	FOREIGN KEY ([ReceiveID]) REFERENCES [tblReceiveNote](ID),
+)
+GO
